780970285b35269f0ba8d9e1d8f9a6ebec8d6da1
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-11 00:58:59 +0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Toolbar from './components/Toolbar';
 import Content from './components/Content';
 import Home from './pages/Home';
 import About from './pages/About';
 import Books from './pages/Books';
+import Book from './pages/Book';
+import Sidenav from './components/Sidenav';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound'
 
 class App extends Component {
     render() {
@@ -14,12 +17,16 @@ class App extends Component {
             <Router>
                 <div className="app">
                     <Toolbar />
-
                     <Content>
-                        <Route path="/" exact component={Home} />
-                        <Route path="/about" component={About} />
-                        <Route path="/books" component={Books} />
-                        <Route path="/login" component={Login} />
+                        <Route path="/books" component={Sidenav} />
+                        <Switch>
+                            <Route path="/" exact component={Home} />
+                            <Route path="/about" component={About} />
+                            <Route path="/books/:topic?" exact component={Books} />
+                            <Route path="/books/:topic/:book" component={Book} />
+                            <Route path="/login" component={Login} />
+                            <Route component={NotFound} />
+                        </Switch>
                     </Content>
                 </div>
             </Router>
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 import topics from '../data/topics';
 
 function Sidenav() {
     return (
         <aside className="mdc-permanent-drawer">
             <nav className="mdc-list mdc-list--avatar-list">
-                {topics.map(topic => 
-                    <a
+                {topics.map(topic =>
+                    <NavLink
                         key={topic.id}
-                        href={`/books/${topic.id}`}
-                        className="mdc-list-item">
+                        to={`/books/${topic.id}`}
+                        className="mdc-list-item"
+                        activeClassName="mdc-temporary-drawer--selected">
                         <img src={`/img/topics/${topic.id}.svg`} className="mdc-list-item__start-detail"/>
                             {topic.title}
-                    </a>
+                    </NavLink>
                 )}
             </nav>
         </aside>
